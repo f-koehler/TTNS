79876d9daad6a7f769e527dcaa99da5756fa546a
Fabian Köhler <fabian.koehler@protonmail.ch>
2023-05-08 20:59:55 +0200

    Test child number for tree.

--- tests/test_tree.cpp
+++ tests/test_tree.cpp
@@ -17,24 +17,35 @@ TEST_CASE("BFSIterator", "[tree]") {
   root.emplace_child(2);
   root.emplace_child(3);
   root.emplace_child(4);
+  REQUIRE(root.get_num_children() == 3);
 
   root[0].emplace_child(5);
   root[0].emplace_child(6);
+  REQUIRE(root[0].get_num_children() == 2);
+
   root[2].emplace_child(7);
   root[2].emplace_child(8);
+  REQUIRE(root[2].get_num_children() == 2);
 
   root[0][0].emplace_child(9);
   root[0][0].emplace_child(10);
+  REQUIRE(root[0][0].get_num_children() == 2);
+
   root[2][0].emplace_child(11);
   root[2][0].emplace_child(12);
+  REQUIRE(root[2][0].get_num_children() == 2);
 
-  std::vector<int> result(12);
-  std::transform(root.begin(), root.end(), result.begin(),
-                 [](Node &node) { return node.get_data(); });
-  REQUIRE(result == std::vector<int>{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12});
+  SECTION("begin() and end()") {
+    std::vector<int> result;
+    std::transform(root.begin(), root.end(), std::back_inserter(result),
+                   [](Node &node) { return node.get_data(); });
+    REQUIRE(result == std::vector<int>{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12});
+  }
 
-  std::vector<int> result2(12);
-  std::transform(root.cbegin(), root.cend(), result2.begin(),
-                 [](const Node &node) { return node.get_data(); });
-  REQUIRE(result2 == std::vector<int>{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12});
+  SECTION("cbegin() and cend()") {
+    std::vector<int> result;
+    std::transform(root.cbegin(), root.cend(), std::back_inserter(result),
+                   [](const Node &node) { return node.get_data(); });
+    REQUIRE(result == std::vector<int>{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12});
+  }
 }
